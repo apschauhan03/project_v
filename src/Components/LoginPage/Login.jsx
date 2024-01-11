c9b928d665cefca6d8a23c280680afda60960e45
Abhijit Singh <abhijitchau8948@gmail.com>
2024-01-12 04:02:33 +0530

    added state for credentials

--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -3,22 +3,48 @@ import styles from "./Login.module.css";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 function Login() {
-    const [togglePassword,setTogglePassword] = useState(false);
-    const togglePasswordHandler = ()=>{
-        setTogglePassword(!togglePassword);
-    }
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const credentialsChangeHandler = (event) => {
+    setCredentials((prevValue)=>{
+      return {...prevValue,[event.target.name]:event.target.value};
+    })
+  };
+  const togglePasswordHandler = () => {
+    setTogglePassword(!togglePassword);
+  };
+  const onSubmitHandler = ()=>{
+    console.log("helo");
+    console.log(credentials);
+  }
   return (
     <div className={styles.LoginPage}>
       <div className={styles.loginCredentials}>
         <div className={styles.individualCredentials}>
-          <input placeholder="Email"></input>
+          <input
+            onChange={credentialsChangeHandler}
+            name="email"
+            value={credentials.email}
+            placeholder="Email"
+          ></input>
         </div>
         <div className={styles.individualCredentials}>
-          <input type={togglePassword?"text":"password"} placeholder="Password"/>
-          <span onClick={togglePasswordHandler} className={styles.showPassword}>{togglePassword?<FaRegEyeSlash />:<FaRegEye/>}</span>
+          <input
+          name="password"
+            onChange={credentialsChangeHandler}
+            value={credentials.password}
+            type={togglePassword ? "text" : "password"}
+            placeholder="Password"
+          />
+          <span onClick={togglePasswordHandler} className={styles.showPassword}>
+            {togglePassword ? <FaRegEyeSlash /> : <FaRegEye />}
+          </span>
         </div>
         <div className={styles.loginButton}>
-          <button>Submit</button>
+          <button onClick={onSubmitHandler}>Submit</button>
         </div>
       </div>
     </div>
